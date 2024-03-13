6f19816b7ba822ab5419441f96b35cc1245ccff3
alysvolatile <alice.g.cooper@gmail.com>
2024-03-13 13:53:05 -0400

    set up basic form, basic divs, added event listener to generate button

---
+++ app.js
@@ -0,0 +1,9 @@
+const generateButton = document.querySelector('#generate')
+
+const GenerateGrid = () => {
+    alert("ClickyClick")
+}
+
+generateButton.addEventListener('click', ()=>{ 
+    GenerateGrid()
+})
